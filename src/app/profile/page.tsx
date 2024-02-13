5b77f886cd0a88ba68799d9af6ecee9fe4902bdc
patelabhay12 <abhaypatel6794@gmail.com>
2024-02-13 19:47:08 +0530

    Routing Middleware Page protection

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,10 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function Profile() {
     const router = useRouter();
-
+    const [data, setData] = useState("Nothing");
     const Logout = async () => {
         try {
             await axios.get("api/users/logout");
@@ -13,17 +14,26 @@ export default function Profile() {
         } catch (error: any) {
             console.log(error.message);
         }
-    }
+    } 
 
+ 
+    const getUserDetails = async () => {
+        const user = await axios.get("api/users/me");
+        setData(user.data.data._id);
+    }  
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page...</p>
             <hr />
+            <h2 className="p-3 rounded bg-green-500">{data === "nothing" ? "nothing" : <Link href={`/profile/${data}`}>{ data}</Link>}</h2>
             <button
                 onClick={Logout}
                 className="bg-blue-500 border mt-4 hover::bg-blue-700 text-black font-bold py-2 px-4 rounded">Logout</button>
+            <button
+                onClick={getUserDetails}
+                className="bg-green-500 border mt-4 hover::bg-blue-700 text-black font-bold py-2 px-4 rounded">GetUse Details</button>
         </div>
     )
 }; 
