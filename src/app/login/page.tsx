3022ef20b50ad9311979d90058fcafcfc970d14e
patelabhay12 <abhaypatel6794@gmail.com>
2024-02-13 18:44:20 +0530

    Logout Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,45 @@
 "use client"
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
+import { findAllInRenderedTree } from "react-dom/test-utils";
 
 export default function Login() {
-
+    const router = useRouter();
     const [user, setUser] = useState({
         email: "",
         password: ""
     });
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const onLogin = async () => {
-        
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log(response);
+            router.push("/profile")
+        } catch (error: any) {
+            console.log("Login Failed", error.message);
+        }
+        finally {
+            setLoading(false);
+        }
     }
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+
+    }, [user]);
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1>Login Page....</h1>
-        
+            <h1>{loading ? "Loading..." : "Login Page...."}</h1>
+
             <hr />
             <label htmlFor="email">email</label>
             <input
@@ -26,7 +48,7 @@ export default function Login() {
                 type="text"
                 value={user.email}
                 placeholder="username"
-                onChange={(e)=>setUser({...user,email:e.target.value})}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
             />
             <hr />
             <label htmlFor="password">Password</label>
@@ -36,13 +58,13 @@ export default function Login() {
                 type="text"
                 value={user.password}
                 placeholder="username"
-                onChange={(e)=>setUser({...user,password:e.target.value})}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
             <hr />
             <button
                 onClick={onLogin}
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-            >Login here</button>
+            >{ }Login here</button>
             <Link href="/signup">Visit SignUp page</Link>
         </div>
     )
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,29 @@
 "use client"
-
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function Profile() {
+    const router = useRouter();
+
+    const Logout = async () => {
+        try {
+            await axios.get("api/users/logout");
+            console.log("Logout successfully...");
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page...</p>
+            <hr />
+            <button
+                onClick={Logout}
+                className="bg-blue-500 border mt-4 hover::bg-blue-700 text-black font-bold py-2 px-4 rounded">Logout</button>
         </div>
     )
-};
+}; 
