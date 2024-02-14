0b85173c8bb0e1e488c767cec0bd7bb5bbe62cbe
patelabhay12 <abhaypatel6794@gmail.com>
2024-02-14 22:34:10 +0530

    nodemailer added

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default:false
     },
-    forgotPasswordTOken: String,
+    forgotPasswordToken: String,
     forgotPasswordTokenExpiry: Date,
     verifyToken: String,
     verifyTokenExpiry:Date
